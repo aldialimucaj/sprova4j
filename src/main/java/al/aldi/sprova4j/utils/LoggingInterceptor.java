c5833c0d1dea31253d4856feb952c541c6016e33
Aldi Alimucaj <aldi.alimucaj@gmail.com>
2018-05-15 14:31:21 +0200

    [INFRA] maven push

--- src/main/java/al/aldi/sprova4j/utils/LoggingInterceptor.java
+++ src/main/java/al/aldi/sprova4j/utils/LoggingInterceptor.java
@@ -3,6 +3,7 @@ package al.aldi.sprova4j.utils;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import okio.Buffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,14 +15,25 @@ public class LoggingInterceptor implements Interceptor {
     @Override
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Request request = chain.request();
+        String body = null;
+
+        // not all requests have body.
+        try {
+            final Request copy = request.newBuilder().build();
+            final Buffer buffer = new Buffer();
+            copy.body().writeTo(buffer);
+            body = buffer.readUtf8();
+        } catch (final Exception e) {
+            body = "{}";
+        }
 
         long t1 = System.nanoTime();
-        logger.info(String.format("Sending request %s on %s%n%s", request.url(), chain.connection(), request.headers()));
+        logger.debug(String.format("REQ [%s] %s on %n%s %s", request.method(),  request.url(), request.headers(), body));
 
         Response response = chain.proceed(request);
 
         long t2 = System.nanoTime();
-        logger.info(String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+        logger.debug(String.format("RES %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d, response.headers()));
 
         return response;
     }
